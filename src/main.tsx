5a5c02cfcef6772c67b5e495f1799fbfd68e301b
toufiq-tech <toufiqur10rahaman@gmail.com>
2026-09-13 20:21:14 +0600

    Stack Counter Added

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,6 @@ import { ToastContainer } from 'react-toastify';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <ToastContainer
-      position="top-center"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
+    <ToastContainer/>
   </StrictMode>,
 )
